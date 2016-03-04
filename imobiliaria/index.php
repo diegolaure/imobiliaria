79e9468d552051c568967340f939e4b5e6128723
Diego Laure <diegolaure7@gmail.com>
2016-03-04 15:35:48 -0300

    Alteração da pagina inicial do projeto

--- imobiliaria/index.php
+++ imobiliaria/index.php
@@ -1,1 +1,11 @@
-<?php
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8" />
+	<title>Projeto Imobiliaria</title>
+</head>
+<body>
+<h1>Projeto Imobiliaria</h1>
+<p>Alteraçoes no projeto</p> 	
+</body>
+</html>
